626a7fbfce694f624d76735abae12ce442950510
Олексій2930 <korobcukdenis30@gmail.com>
2026-07-01 12:45:26 +0300

    validation-zod-server

---
+++ app/lib/schema.ts
@@ -0,0 +1,8 @@
+
+import  {z} from 'zod';
+
+export const userSchema = z.object({
+    name: z.string().min(2, '2 min length'),
+    email: z.string().email('Invalid email address'),
+});
+export type UserForm = z.infer<typeof userSchema>;
